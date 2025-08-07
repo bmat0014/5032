772c97d885725db866faa8855fc49b521ec56a5f
Blake Matheson <bmat0014@student.monash.edu>
2025-08-07 14:30:56 +1000

    Task 1 completed

---
+++ FIT5032-WEEK1/src/components/myName.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h3>Blake</h3>
+</template>
+
+<style scoped>
+  h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+  }
+</style>
