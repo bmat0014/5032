4627909229644a94d2773b6f78ccb7569b846daf
Blake Matheson <bmat0014@student.monash.edu>
2025-09-19 22:24:39 +1000

    task 8.2 half completed

--- FIT5032-WEEK5/NoMash-Library-main/src/components/BookList.vue
+++ FIT5032-WEEK5/NoMash-Library-main/src/components/BookList.vue
@@ -2,14 +2,18 @@
   <div>
     <h1>Books with ISBN > 1000</h1>
     <ul>
-      <li v-for="book in books" :key="book.id">{{ book.name }} - ISBN: {{ book.isbn }}</li>
+      <li v-for="book in books" :key="book.id">
+        {{ book.name }} - ISBN: {{ book.isbn }}
+        <button @click="updateBook(book.id)" style="margin-left: 10px">Update</button>
+        <button @click="deleteBook(book.id)" style="margin-left: 5px">Delete</button>
+      </li>
     </ul>
   </div>
 </template>
 <script>
 import { ref, onMounted } from 'vue'
 import db from '../Firebase/init.js'
-import { collection, query, where, getDocs } from 'firebase/firestore'
+import { collection, query, where, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore'
 
 export default {
   setup() {
@@ -29,12 +33,41 @@ export default {
       }
     }
 
+    const updateBook = async (bookId) => {
+      // Simple prompt for now - you could make this more sophisticated
+      const newName = prompt('Enter new book name:')
+      const newIsbn = prompt('Enter new ISBN:')
+
+      if (newName && newIsbn) {
+        try {
+          await updateDoc(doc(db, 'books', bookId), {
+            name: newName,
+            isbn: Number(newIsbn)
+          })
+          fetchBooks()
+        } catch (error) {
+          console.error('Error updating book:', error)
+        }
+      }
+    }
+
+    const deleteBook = async (bookId) => {
+      try {
+        await deleteDoc(doc(db, 'books', bookId))
+        fetchBooks()
+      } catch (error) {
+        console.error('Error deleting book:', error)
+      }
+    }
+
     onMounted(() => {
       fetchBooks()
     })
 
     return {
-      books
+      books,
+      updateBook,
+      deleteBook
     }
   }
 }
